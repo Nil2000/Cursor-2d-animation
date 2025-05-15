002f240fbbc00a1e724612a82f87f22f0db90fb4
NIL2000 <nilabhra09b.net@gmail.com>
2025-05-15 23:44:00 +0530

    Fixed hydration error for themes

--- main-app/app/_component/navbar.tsx
+++ main-app/app/_component/navbar.tsx
@@ -1,11 +1,14 @@
+"use client";
 import React from "react";
 import NavbarAuthStatus from "./navbar-components/navbar-auth-status";
+import ThemeButton from "@/components/theme-button";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-4 w-full lg:w-[1000px] mx-auto h-16">
+    <nav className="flex items-center justify-between p-4 w-full h-16">
       <div>AniX</div>
-      <div>
+      <div className="flex gap-2">
+        <ThemeButton />
         <NavbarAuthStatus />
       </div>
     </nav>
--- main-app/app/page.tsx
+++ main-app/app/page.tsx
@@ -1,5 +1,10 @@
 import Navbar from "./_component/navbar";
 
 export default function Home() {
-  return <Navbar />;
+  return (
+    <div className="w-full lg:w-[1000px] mx-auto">
+      <Navbar />
+      <div>Animate Your Imagination</div>
+    </div>
+  );
 }
