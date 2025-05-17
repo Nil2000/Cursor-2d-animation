7293f5abf9b0af0b180b81d130e6bf41817d63cd
NIL2000 <nilabhra09b.net@gmail.com>
2025-05-18 00:55:31 +0530

    Sidebar and navbar added to chat space

---
+++ main-app/app/(normal)/_components/chat-navbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import ThemeButton from "@/components/theme-button";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
+import { PanelLeftClose, PanelLeftOpen } from "lucide-react";
+import React from "react";
+
+export default function ChatNavbar() {
+  const { open, setOpen } = useSidebar();
+  return (
+    <nav className="h-16 flex items-center justify-between p-4 w-full">
+      <div>
+        <Button
+          onClick={() => setOpen(!open)}
+          variant="outline"
+          className="h-9 w-9"
+        >
+          {open ? <PanelLeftClose size={16} /> : <PanelLeftOpen size={16} />}
+        </Button>
+      </div>
+      <div>
+        <ThemeButton />
+      </div>
+    </nav>
+  );
+}
